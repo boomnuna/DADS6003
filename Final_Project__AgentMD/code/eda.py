cb361e5837d8cb0f404f1846103e7f6b4a397718
anbun_ <boomnuna@gmail.com>
2026-05-25 16:30:08 +0700

    add new file

---
+++ Final_Project__AgentMD/code/eda.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+
+# load data 
+data = pd.read_csv(r'C:\Users\T00229\Documents\GitHub\DADS6003\Final_Project__AgentMD\dataset\creditcard_train.csv')
+
+# visualization
+
+
+
+
